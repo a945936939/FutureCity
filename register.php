1276cc2a2a8c75f7363163b419ded0ba8fc02338
ThomasJackson1 <101170517+ThomasJackson1@users.noreply.github.com>
2022-05-14 02:05:46 +1000

    Completed register page
    Register page changed to give the user a username and password, no longer lets user choose the username

--- register.php
+++ register.php
@@ -2,91 +2,102 @@
 // require_once "config.php";
 require_once "mysql.php";
 require_once "connection.php";
+   
+$link = $conn;
 
-if(isset($_POST['submit'])){
-    // Connect to the database
-    $link = $conn;
-
-    // Get username and password
-    $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
-
-    preg_match("/^[A-Za-z0-9]*$/",$username,$u_match);
-    preg_match("/^[A-Za-z0-9]*$/",$username,$p_match);
-    if (!$u_match || !$p_match) {
-        echo "<script>alert('Please type in letters and numbers');location.href='register.php';</script>";
-    }else if(strlen($password) < 4){
-        header("Location:register.php");
-    };
-
-    
-    $username = escape($username);
-    // encrypt the password
-    $password = hash("sha256",$password);
-
-    $query = "select user_id from user_profile where user_id={$username}";
-    $res = execute($link,$query);
-    if(!is_null(sqlsrv_fetch_array($res))){
-        echo "<script>alert('That account already exists');location.href='register.php';</script>";
-        die;
-    };
-
-
-    // create the account
-    $query = "insert into user_profile (user_id, date_registered, hashed_password) values({$username}, CURRENT_TIMESTAMP,'{$password}')";
-    $res = execute($link,$query);
-    if(sqlsrv_num_rows($res) !== 0){
-        echo "<script>alert('Your account is created');location.href='index.php';</script>";
-    };
-}
 ?>
 <!DOCTYPE html>
 <html lang="zh">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Register</title>
     <link rel="stylesheet" href="login.css">
+    <!-- <script src="js/jquery-2.1.1.min.js"></script> -->
+
+<!-- 
+    <script type="text/javascript">
+        // track whether button has been clicked
+    
+    </script> -->
+
 </head>
 
 <body>
-<div id="wrapper" class="login-page">
-    <div id="login_form" class="form">
-        <form action="" class="login-form" method="post">
-            <input type="text" placeholder="username" id="user_name" name="username"/>
-            <input type="password" placeholder="password" id="password" name="password"/>
-            <input type="password" placeholder="confirm the password" id="password_confirm" name="password_confirm"/>
-            <button id="create" name="submit" type="submit">Register</button>
-            <p class="message">Already have an account?<a href="index.php">Login</a></p>
+<div id="wrapper" class="register-page">
+
+    <div id="reg_form" class="form">
+        <button id="Register" name="submit">Register</button>
+
+        <p id= "uname">Your username:</p>
+        
+        <p id = "pword">Your password:</p>
+      
+        <form name ="submit" type = "submit" action="change.php">
+            <button type="submit" >Change your password</button>
+           
         </form>
+
+        <form name ="submit" type = "submit" action="index.php">
+        <button type="submit" >Back to login</button>
+        </form>
+
+        <!-- <a herf = ><button id="change" name="submit" type="submit">Change your password</button></a>
+        <a herf = "login.php"><button id="login" name="submit" type="submit">Back to login</button></a>  -->
+
     </div>
 </div>
 
-<script src="js/jquery-2.1.1.min.js"></script>
-<script type="text/javascript">
-    $("#create").on('click',function(e){
-        let username = $("#user_name").val().trim();
-        let password = $("#password").val().trim();
-        let password_confirm = $("#password_confirm").val().trim();
-        let reg =  /^[A-Za-z0-9]*$/;
 
-        if (username === "" || password === ""){
-            e.preventDefault();
-            alert('Please type in');
-        }else if (password.length < 4){
-            e.preventDefault();
-            alert('The password is too short');
-        }else if(reg.test(username) === false || reg.test(password) === false){
-            e.preventDefault();
-            alert('Please type in letters and numbers');
-        }else if(password !== password_confirm){
-            e.preventDefault();
-            alert('Please check the password');
-            $("#password").val("");
-            $("#password_confirm").val("");
-        }
-    })
+
+
+<script type="text/javascript"> 
+
+//  to identify first click of the register button
+let first_click = true;
+
+document.getElementById("Register").addEventListener("click", addUser);
+
+// add a user to the database, then display the username and password on the page
+function addUser(){
+
+    
+    // generate password
+    temp_pword = "<?php 
+    $pword = rand(1000, 9999);
+    $hash_pword = hash("sha256", $pword);
+
+    // insert new user account into database
+    $query = "insert into user_profile (user_id, date_registered, hashed_password)
+     values (next value for user_id_gen, CURRENT_TIMESTAMP, '{$hash_pword}');";
+    $result = sqlsrv_query($link, $query);
+    echo ($pword);
+    ?>";
+    
+    // only change elements if it is after the first click
+    if(first_click){
+    // display password
+    document.getElementById("pword").insertAdjacentHTML('beforeend', temp_pword);
+
+    // display username
+    document.getElementById("uname").insertAdjacentHTML('beforeend', "<?php 
+        $query = "select max(user_id) as 'u_id' from user_profile;";
+        $result = sqlsrv_query($link, $query);
+        $result = sqlsrv_fetch_array($result)["u_id"];
+        echo $result;
+    
+    ?>");
+
+    // user cannot click button again
+    first_click = false;
+    };
+}
+
 </script>
+
+
 </body>
+
 </html>
